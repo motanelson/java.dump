d088aad6e777878b5dda991d2cc523555074c4fb
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-27 16:28:27 +0100

    Add files via upload

--- javadump.py
+++ javadump.py
@@ -1,57 +1,71 @@
 import os
 import copy
 print("\033c\033[47;31m/give me the class file to view ? ")
-a=input().strip()
-#a="Hello.class"
+#a=input().strip()
+a="Hello.class"
 f1=open(a,"rb")
 b=f1.read()
 f1.close()
-j=False
-t=False
-l=[]
-pp=0
-ui=bytearray()
-p=False
-o=False
-count=0
-x=True
-for c in b:
-    if t and p and x:
-        if c>31 and c<129:
-            o=True
-        else:
-            o=False
-            if t:
-                x=False
-        p=False    
-    if t and o and x:
-        if c>31 and c<129:
-            cc=chr(c)
-    
-            print(cc,end="")
-            j=True
-    if t and oo and o==False and x==False:
-        ui=ui+bytes([c])   
-    if len(l)==2 and x:
-        l.append(c)
-        i:int=int(c)
-        pp=c
-        print("\n"+str(count)+"   "+str(i)+" " ,end =" ")
-        p=True
-        t=True
-        oo=True
-        count=count+1
-    if c==1 and x:
-        l.clear()
-        l.append(1)
-        oo=False
-        
-    if c==0 and len(l)==1:
-        if l[0]==1:
-            l.append(0)
 
-f1=open("/tmp/out.bin","wb")
-f1.write(ui)
-f1.close()
-os.system("rasm2 -a java -D -B -f /tmp/out.bin")
-   
+def bins(ui):
+    f1=open("/tmp/out.bin","wb")
+    f1.write(ui)
+    f1.close()
+    os.system("rasm2 -a java -D -B -f /tmp/out.bin")
+def texts(counts:int,i:chr):
+    print(i ,end ="")
+    
+
+steps=0
+stepsA=True
+stepsB=False
+ui=bytearray()
+count=0
+counts=0
+for c in b:
+    if steps==3:
+        print()
+        if stepsA:
+            
+            if c>31 and c<129:
+                
+                stepsB=False
+                steps=4
+                
+            else:
+                stepsB=False
+                stepsA=False
+                steps=5
+        else:
+            steps=5
+        
+                
+    if steps==4:
+        texts(counts,chr(c))
+        if count==0:
+            steps=0
+        count=count-1
+    
+    if steps==5:
+        
+        ui=ui+bytes([c])
+        if count==0: 
+            steps=0
+            bins(ui)
+        count=count-1
+    if steps==2:
+        counts=counts+1
+        print("\n"+str(counts),end=" ")
+        count=int(c)
+        stepsB=True
+        print(" "+str(count),end=" ")
+        ui=bytearray()
+        steps=3
+    if steps==1 and c==0:
+        steps=2
+    if steps==0 and c==1:
+        steps=1
+    if counts>2:
+        pass
+        #exit(1)
+bins(ui)  
